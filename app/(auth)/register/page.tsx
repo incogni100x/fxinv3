bfe9192bee8895adac6022b828ded76de784b7c4
Kcokoji <kelechiokoji52@gmail.com>
2024-11-26 01:11:20 +0100

    Add Supabase authentication and session management features

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RegisterForm from "../components/register-form";
+import Register from "@/components/supaauth/register";
 
 export default function RegisterPage() {
-  return <RegisterForm />;
+  return <Register />;
 }
