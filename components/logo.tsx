e6d0dc6bc581167a5d039ed2b6ba07b3e7cbe223
incogni100x <duche20001@gmail.com>
2026-05-10 02:44:31 +0100

    Update site logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,5 +2,13 @@ import Image from "next/image";
 import React from "react";
 
 export default function Logo() {
-  return <Image src="/images/logo.png" alt="logo" width={60} height={60} />;
+  return (
+    <Image
+      src="/images/logo.svg"
+      alt="Pro Elite Holdings"
+      width={60}
+      height={60}
+      priority
+    />
+  );
 }
